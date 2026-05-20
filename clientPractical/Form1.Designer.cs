3cf77f53e7ca392afa028524e2786064f263239e
shahdhassan17 <shodymahdy7@gmail.com>
2026-05-20 18:09:47 +0300

    Merge Server and Client into main branch

---
+++ clientPractical/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace clientPractical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect = new System.Windows.Forms.Button();
+            this.sendInfo = new System.Windows.Forms.Button();
+            this.ready2 = new System.Windows.Forms.Button();
+            this.ready3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(12, 364);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 23);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // sendInfo
+            // 
+            this.sendInfo.Location = new System.Drawing.Point(171, 364);
+            this.sendInfo.Name = "sendInfo";
+            this.sendInfo.Size = new System.Drawing.Size(75, 23);
+            this.sendInfo.TabIndex = 1;
+            this.sendInfo.Text = "send info";
+            this.sendInfo.UseVisualStyleBackColor = true;
+            this.sendInfo.Click += new System.EventHandler(this.sendInfo_Click);
+            // 
+            // ready2
+            // 
+            this.ready2.Location = new System.Drawing.Point(345, 362);
+            this.ready2.Name = "ready2";
+            this.ready2.Size = new System.Drawing.Size(80, 27);
+            this.ready2.TabIndex = 2;
+            this.ready2.Text = "ready2";
+            this.ready2.UseVisualStyleBackColor = true;
+            this.ready2.Click += new System.EventHandler(this.ready2_Click);
+            // 
+            // ready3
+            // 
+            this.ready3.Location = new System.Drawing.Point(546, 360);
+            this.ready3.Name = "ready3";
+            this.ready3.Size = new System.Drawing.Size(80, 27);
+            this.ready3.TabIndex = 3;
+            this.ready3.Text = "ready3";
+            this.ready3.UseVisualStyleBackColor = true;
+            this.ready3.Click += new System.EventHandler(this.ready3_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(313, 297);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(505, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(505, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(505, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(505, 241);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "seat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Dep";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(389, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "section";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 414);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ready3);
+            this.Controls.Add(this.ready2);
+            this.Controls.Add(this.sendInfo);
+            this.Controls.Add(this.connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button sendInfo;
+        private System.Windows.Forms.Button ready2;
+        private System.Windows.Forms.Button ready3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ clientPractical/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clientPractical
+{
+    public partial class Form1 : Form
+    {
+        Socket client;
+        byte[] data = new byte[65536];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Log(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+                richTextBox1.Invoke(new Action(() =>
+                    richTextBox1.AppendText(text + "\n")));
+            else
+                richTextBox1.AppendText(text + "\n");
+        }
+        private static int ReceiveAll(Socket socket, byte[] buffer, int length)
+        {
+            int total = 0;
+            while (total < length)
+            {
+                int received = socket.Receive(
+                    buffer, total, length - total, SocketFlags.None);
+
+                if (received == 0)
+                    throw new SocketException(
+                        (int)SocketError.ConnectionReset);
+
+                total += received;
+            }
+            return total;
+        }
+        private string ReceiveString()
+        {
+            int n = client.Receive(data);
+            return Encoding.ASCII.GetString(data, 0, n);
+        }
+        private async void connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+                client = new Socket( AddressFamily.InterNetwork ,SocketType.Stream,ProtocolType.Tcp);
+                await Task.Run(() => client.Connect(ipep));
+                Log("Connected To Server");
+                string welcome = await Task.Run(() => ReceiveString());
+                Log(welcome);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connect error: " + ex.Message);
+            }
+        }
+        private async void sendInfo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string msg =
+                    textBox1.Text + "|" +
+                    textBox2.Text + "|" +
+                    textBox3.Text + "|" +
+                    textBox4.Text;
+
+                byte[] info = Encoding.ASCII.GetBytes(msg);
+
+                await Task.Run(() => client.Send(info));
+                Log("Info Sent");
+                string echo = await Task.Run(() => ReceiveString());
+                Log("Server : " + echo);
+                string done = await Task.Run(() => ReceiveString());
+                Log(done);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Job1 error: " + ex.Message);
+            }
+        }
+        private async void ready2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await Task.Run(() => client.Send(Encoding.ASCII.GetBytes("#ready2")));
+                Log("Ready2 Sent");
+                byte[] sizeBuffer = new byte[4];
+                await Task.Run(() => ReceiveAll(client, sizeBuffer, 4));
+                int imageSize = BitConverter.ToInt32(sizeBuffer, 0);
+                Log("Image Size = " + imageSize);
+                if (imageSize <= 0 || imageSize > 50 * 1024 * 1024)
+                {
+                    MessageBox.Show("Invalid image size: " + imageSize);
+                    return;
+                }
+                byte[] imageData = new byte[imageSize];
+                await Task.Run(() => ReceiveAll(client, imageData, imageSize));
+                string path = Application.StartupPath + "\\received.jpg";
+                await Task.Run(() => File.WriteAllBytes(path, imageData));
+                Log("Image Received");
+                await Task.Run(() =>
+                    client.Send(Encoding.ASCII.GetBytes(path)));
+                string done = await Task.Run(() => ReceiveString());
+                Log(done);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Job2 error: " + ex.Message);
+            }
+        }
+        private async void ready3_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Send(Encoding.ASCII.GetBytes("#ready3"));
+                Log("Ready3 Sent");
+                string msg = await Task.Run(() => ReceiveString());
+                string[] parts = msg.Split(new char[] { '|' }, 2);
+                string path = Application.StartupPath + parts[0];
+                File.WriteAllText(path, parts[1]);
+                Log("File Saved: " + path);
+                client.Send(Encoding.ASCII.GetBytes(path));
+                string done = await Task.Run(() => ReceiveString());
+                Log(done);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Job3 error: " + ex.Message);
+            }
+        }
+    }
+}
