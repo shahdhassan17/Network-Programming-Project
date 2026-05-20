f87b8a023fba7aa72ae682fc352130a8e8dccf72
shahdhassan17 <148322536+shahdhassan17@users.noreply.github.com>
2026-05-20 18:47:13 +0300

    Add files via upload

---
+++ clientproject/clientproject/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace clientproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect = new System.Windows.Forms.Button();
+            this.Browse = new System.Windows.Forms.Button();
+            this.send = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(12, 335);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 64);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(200, 335);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 64);
+            this.Browse.TabIndex = 1;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(444, 335);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 64);
+            this.send.TabIndex = 2;
+            this.send.Text = "send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(662, 335);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(75, 64);
+            this.close.TabIndex = 3;
+            this.close.Text = "close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(412, 280);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(600, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(507, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ clientproject/clientproject/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace clientproject
+{
+    public partial class Form1 : Form
+    {
+        Socket client;
+
+        byte[] buffer = new byte[65536];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+
+                client.Connect("127.0.0.1", 5000);
+
+                richTextBox1.AppendText(
+                    "Connected To Server\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op =
+                new OpenFileDialog();
+
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = op.FileName;
+            }
+        }
+
+        private void ReceiveAll(byte[] data, int size)
+        {
+            int total = 0;
+
+            while (total < size)
+            {
+                int r = client.Receive(
+                    data,
+                    total,
+                    size - total,
+                    SocketFlags.None);
+
+                total += r;
+            }
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = textBox1.Text;
+
+                if (!File.Exists(path))
+                {
+                    MessageBox.Show(
+                        "Choose File First");
+
+                    return;
+                }
+
+                byte[] fileData =
+                    File.ReadAllBytes(path);
+
+                byte[] size =
+                    BitConverter.GetBytes(
+                        fileData.Length);
+
+                client.Send(size);
+
+                client.Send(fileData);
+
+                richTextBox1.AppendText(
+                    "File Sent ✔\n");
+
+                byte[] compressedSize =
+                    new byte[4];
+
+                ReceiveAll(compressedSize, 4);
+
+                int sizeCompressed =
+                    BitConverter.ToInt32(
+                        compressedSize, 0);
+
+                richTextBox1.AppendText(
+                    "Compressed Size: " +
+                    sizeCompressed + "\n");
+
+                byte[] compressedData =
+                    new byte[sizeCompressed];
+
+                ReceiveAll(
+                    compressedData,
+                    sizeCompressed);
+
+                string savePath =
+                    "compressed_file.gz";
+
+                File.WriteAllBytes(
+                    savePath,
+                    compressedData);
+
+                richTextBox1.AppendText(
+                    "Compressed File Saved ✔\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client?.Close();
+            }
+            catch { }
+
+            richTextBox1.AppendText(
+                "Client Closed\n");
+        }
+    }
+}
